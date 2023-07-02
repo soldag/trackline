8e2cec701931ec14cdc2ae55f02ff78c8a9d2b09
Sören Oldag <soeren_oldag@freenet.de>
2023-07-02 21:34:38 +0200

    Add loader to RootContainer

--- frontend/src/components/containers/RootContainer.jsx
+++ frontend/src/components/containers/RootContainer.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import FullscreenContainer from "components/containers/FullscreenContainer";
 import RoutingContainer from "components/containers/RoutingContainer";
 import SpotifyContainer from "components/containers/SpotifyContainer";
+import LoadingView from "components/views/LoadingView";
 import {
   fetchCurrentUser as fetchCurrentApiUser,
   invalidateSession,
@@ -16,7 +17,9 @@ import { useMountEffect } from "utils/hooks";
 const RootContainer = () => {
   const dispatch = useDispatch();
   const apiSessionToken = useSelector((state) => state.auth.sessionToken);
+  const apiUser = useSelector((state) => state.auth.user);
   const spotifyAccessToken = useSelector((state) => state.spotify.accessToken);
+  const spotifyUser = useSelector((state) => state.spotify.user);
 
   useMountEffect(() => {
     if (apiSessionToken) {
@@ -31,6 +34,10 @@ const RootContainer = () => {
       dispatch(invalidateAccessToken());
     }
   });
+
+  if ((apiSessionToken && !apiUser) || (spotifyAccessToken && !spotifyUser)) {
+    return <LoadingView />;
+  }
 
   return (
     <FullscreenContainer>
