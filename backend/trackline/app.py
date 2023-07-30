f15bd669c6580d652baaa78fd4efe9285f9b2dd2
Sören Oldag <soeren_oldag@freenet.de>
2023-07-30 14:40:32 +0200

    Replace exception handlers with custom middleware
    This allows to also handle errors occurring in other middlewares

--- backend/trackline/app.py
+++ backend/trackline/app.py
@@ -1,7 +1,7 @@
 import logging
 
-from fastapi import Depends, FastAPI, Request, WebSocket
-from fastapi.exceptions import RequestValidationError, WebSocketRequestValidationError
+from fastapi import Depends, FastAPI, WebSocket
+from fastapi.exceptions import WebSocketRequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_injector import attach_injector, InjectorMiddleware
 from injector import Injector
@@ -9,15 +9,13 @@ from injector import Injector
 from trackline.auth.deps import websocket_logger
 from trackline.auth.router import router as auth_router
 from trackline.core.di import CoreModule
-from trackline.core.exceptions import RequestException
 from trackline.core.middleware import (
     DatabaseTransactionMiddleware,
+    ExceptionHandlingMiddleware,
     LoggingMiddleware,
     NoIndexMiddleware,
     ServerTimeMiddleware,
 )
-from trackline.core.schemas import ErrorDetail
-from trackline.core.utils.response import make_error
 from trackline.games.di import GamesModule
 from trackline.games.router import router as games_router
 from trackline.spotify.client import SpotifyClient
@@ -37,6 +35,12 @@ app = FastAPI(
 injector = Injector([CoreModule(), GamesModule(), SpotifyModule()])
 attach_injector(app, injector)
 
+app.add_middleware(DatabaseTransactionMiddleware, injector=injector)
+app.add_middleware(InjectorMiddleware, injector=injector)
+app.add_middleware(ExceptionHandlingMiddleware)
+app.add_middleware(ServerTimeMiddleware)
+app.add_middleware(NoIndexMiddleware)
+app.add_middleware(LoggingMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins="*",
@@ -45,11 +49,6 @@ app.add_middleware(
     allow_headers=["*"],
     expose_headers=["x-server-time"],
 )
-app.add_middleware(LoggingMiddleware)
-app.add_middleware(ServerTimeMiddleware)
-app.add_middleware(NoIndexMiddleware)
-app.add_middleware(DatabaseTransactionMiddleware, injector=injector)
-app.add_middleware(InjectorMiddleware, injector=injector)
 
 app.include_router(auth_router)
 app.include_router(games_router)
@@ -75,42 +74,6 @@ async def on_shutdown():
     await spotify_client.close()
 
     log.info("Application shutdown complete.")
-
-
-@app.exception_handler(Exception)
-async def global_exception_handler(_, exc: Exception):
-    return make_error(
-        code="UNEXPECTED_ERROR",
-        message="An unexpected error occurred.",
-        status_code=500,
-    )
-
-
-@app.exception_handler(RequestException)
-async def request_exception_handler(
-    request: Request | WebSocket, exc: RequestException
-):
-    if isinstance(request, WebSocket):
-        await request.close(code=1008, reason=exc.code)
-    else:
-        return make_error(
-            code=exc.code,
-            message=exc.message,
-            status_code=exc.status_code,
-        )
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(_, exc: RequestValidationError):
-    return make_error(
-        code="VALIDATION_ERROR",
-        message="The request is invalid.",
-        details=[
-            ErrorDetail(message=err["msg"], location=err.get("loc"))
-            for err in exc.errors()
-        ],
-        status_code=400,
-    )
 
 
 @app.exception_handler(WebSocketRequestValidationError)
--- backend/trackline/core/middleware.py
+++ backend/trackline/core/middleware.py
@@ -4,13 +4,18 @@ import logging
 import random
 import time
 
+from fastapi.exceptions import RequestValidationError
 from injector import Injector
 from pymongo.errors import OperationFailure
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
 from starlette.responses import Response
+from starlette.types import ASGIApp, Receive, Scope, Send
+from starlette.websockets import WebSocket
 
 from trackline.core.db.client import DatabaseClient
+from trackline.core.exceptions import RequestException
+from trackline.core.schemas import ErrorDetail
 from trackline.core.settings import Settings
 from trackline.core.utils.datetime import utcnow
 from trackline.core.utils.response import make_error
@@ -53,6 +58,61 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         )
 
         return response
+
+
+class ExceptionHandlingMiddleware:
+    def __init__(self, app: ASGIApp):
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        match scope["type"]:
+            case "http":
+                await self._handle_request(scope, receive, send)
+            case "websocket":
+                await self._handle_websocket(scope, receive, send)
+            case _:
+                await self.app(scope, receive, send)
+
+    async def _handle_request(self, scope: Scope, receive: Receive, send: Send) -> None:
+        try:
+            await self.app(scope, receive, send)
+        except RequestValidationError as exc:
+            await make_error(
+                code="VALIDATION_ERROR",
+                message="The request is invalid.",
+                details=[
+                    ErrorDetail(message=err["msg"], location=err.get("loc"))
+                    for err in exc.errors()
+                ],
+                status_code=400,
+            )(scope, receive, send)
+        except RequestException as exc:
+            await make_error(
+                code=exc.code,
+                message=exc.message,
+                status_code=exc.status_code,
+            )(scope, receive, send)
+        except Exception:
+            log.exception("Unhandled exception", exc_info=True)
+            await make_error(
+                code="UNEXPECTED_ERROR",
+                message="An unexpected error occurred.",
+                status_code=500,
+            )(scope, receive, send)
+
+    async def _handle_websocket(
+        self, scope: Scope, receive: Receive, send: Send
+    ) -> None:
+        ws = WebSocket(scope, receive, send)
+        try:
+            await self.app(scope, receive, send)
+        except RequestValidationError:
+            await ws.close(code=1008, reason="VALIDATION_ERROR")
+        except RequestException as exc:
+            await ws.close(code=1008, reason=exc.code)
+        except Exception:
+            log.exception("Unhandled exception", exc_info=True)
+            await ws.close(code=1008)
 
 
 class DatabaseTransactionMiddleware(BaseHTTPMiddleware):
