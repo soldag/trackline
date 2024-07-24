ae2ebd221098f04e94a7342d6a3b929712f404af
Sören Oldag <soeren_oldag@freenet.de>
2024-07-25 00:06:47 +0200

    Remove deprecated options from migrate-mongo config

--- database/migrate-mongo-config.js
+++ database/migrate-mongo-config.js
@@ -1,15 +1,7 @@
 const config = {
   mongodb: {
     url: process.env.MONGO_URL || "mongodb://localhost:27017",
-
     databaseName: "trackline",
-
-    options: {
-      useNewUrlParser: true, // removes a deprecation warning when connecting
-      useUnifiedTopology: true, // removes a deprecating warning when connecting
-      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
-      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
-    },
   },
 
   // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
