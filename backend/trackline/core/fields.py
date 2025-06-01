57fd0d67902a77dfb4b0f03b955e481f94dacfaa
Sören Oldag <soeren_oldag@freenet.de>
2025-06-02 00:48:11 +0200

    Remove unused ResourceIdOld

--- backend/trackline/core/fields.py
+++ backend/trackline/core/fields.py
@@ -1,54 +1,10 @@
-from typing import Annotated, Any
+from typing import Annotated
 
 from beanie import PydanticObjectId
-from bson import ObjectId
-from pydantic import Field, GetCoreSchemaHandler, GetJsonSchemaHandler
-from pydantic.json_schema import JsonSchemaValue
-from pydantic_core import core_schema, CoreSchema
+from pydantic import Field
 
 
 Fraction = Annotated[float, Field(ge=0, le=1)]
 
 
 ResourceId = PydanticObjectId
-
-
-class ResourceIdOld(ObjectId):
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls,
-        source: type[Any],
-        handler: GetCoreSchemaHandler,
-    ) -> core_schema.CoreSchema:
-        from_str_schema = core_schema.chain_schema(
-            [
-                core_schema.str_schema(),
-                core_schema.no_info_plain_validator_function(ObjectId),
-            ]
-        )
-
-        return core_schema.json_or_python_schema(
-            json_schema=from_str_schema,
-            python_schema=core_schema.union_schema(
-                [
-                    core_schema.is_instance_schema(ObjectId),
-                    from_str_schema,
-                ]
-            ),
-            serialization=core_schema.plain_serializer_function_ser_schema(
-                str,
-                when_used="json",
-            ),
-        )
-
-    @classmethod
-    def __get_pydantic_json_schema__(
-        cls,
-        _core_schema: CoreSchema,
-        handler: GetJsonSchemaHandler,
-    ) -> JsonSchemaValue:
-        json_schema = handler(core_schema.str_schema())
-        json_schema["example"] = "64331790c4f8cec3ba2a6662"
-        json_schema["format"] = "regex"
-        json_schema["pattern"] = "^[A-Fa-f0-9]{24}$"
-        return json_schema
