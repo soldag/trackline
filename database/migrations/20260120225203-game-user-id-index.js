3aa734972be42605ded94385396dea3ca621e274
Sören Oldag <soeren_oldag@freenet.de>
2026-01-20 23:59:54 +0100

    Add index to speed up games by user query

---
+++ database/migrations/20260120225203-game-user-id-index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async up(db, client) {
+    await db
+      .collection("game")
+      .createIndex({ "player.user_id": 1 }, { name: "player_user_id_index" });
+  },
+
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async down(db, client) {
+    await db.collection("game").dropIndex("player_user_id_index");
+  },
+};
