4d434a24307ac6b54b1aaa348dfbf5e1fa91a17f
Sören Oldag <soeren.oldag@proton.me>
2026-07-21 17:28:08 +0200

    Configure sentry to ignore network errors

--- frontend/src/instrument.ts
+++ frontend/src/instrument.ts
@@ -1,9 +1,12 @@
 import * as Sentry from "@sentry/react";
 
+import { NetworkError } from "@/api/utils/errors";
 import { SENTRY_DSN } from "@/configuration";
 
 if (SENTRY_DSN) {
   Sentry.init({
     dsn: SENTRY_DSN,
+    beforeSend: (event, hint) =>
+      hint.originalException instanceof NetworkError ? null : event,
   });
 }
